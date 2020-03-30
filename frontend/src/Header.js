502cdbd2794c1f7cfaf5181a14c30bb25a32f833
Isac Moura <isac_moura@atlantico.com.br>
2020-03-30 01:30:24 -0300

    Frontend da aplicação

---
+++ frontend/src/Header.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function Header(props) {
+    return (
+        <header>
+            <h1>Be The Hero</h1>
+        </header>
+    );
+}
+
+export default Header;
