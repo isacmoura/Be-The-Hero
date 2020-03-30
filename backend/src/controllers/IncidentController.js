fcc3c37cf56604211ad2096bae6683c7af1cc24d
Isac Moura <isac_moura@atlantico.com.br>
2020-03-30 01:30:08 -0300

    Correções no backend

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -36,8 +36,8 @@ module.exports = {
         const { id } = request.params;
         const ong_id = request.headers.authorization;
 
-        const incident = connection('incidents').where('id', id).select('ong_id').first();
-
+        const incident = await connection('incidents').where('id', id).select('ong_id').first();
+        
         if(incident.ong_id != ong_id) {
             return response.status(401).json({ error: 'Unauthorized' });
         }
